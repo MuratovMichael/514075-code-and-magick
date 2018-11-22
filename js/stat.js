ed7716a0c802bab6358817d27cfc88becc589132
Mikhail <mikekrd@yandex.ru>
2018-11-22 06:54:17 +0300

    Внёс правки

--- js/stat.js
+++ js/stat.js
@@ -12,23 +12,23 @@ var BAR_HEIGHT = 150;
 var GAP = 10;
 var TEXT_GAP = 15;
 
-
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
-var congratulation = function (ctx) {
+var renderCongratulation = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP + 40, CLOUD_Y + GAP + TEXT_GAP);
   ctx.fillText('Список результатов', CLOUD_X + GAP + 40, CLOUD_Y + 2 * (GAP + TEXT_GAP));
 }
 
-var renderHistograms = function (ctx) {
-  players.forEach(function(i) {
+var renderHistograms = function (ctx, players, times) {
+  players.forEach(function(elem, i) {
     var height = (BAR_HEIGHT * times[i]) / maxTime;
     var roundTime = Math.round(times[i]);
+    var maxTime = getMaxElement(times);
     var sizes = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
 
     ctx.font = '16px PT Mono';
@@ -36,8 +36,7 @@ var renderHistograms = function (ctx) {
     ctx.fillText(players[i], sizes, CLOUD_HEIGHT);
     ctx.fillText(roundTime.toString(), sizes, CLOUD_HEIGHT - height - TEXT_GAP);
 
-
-    players[i] === 'Вы' ? ctx.fillStyle ='rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0,	0, 255,' + Math.random() + ')';
+    ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + Math.random() + ')';
 
     ctx.fillRect(sizes, CLOUD_HEIGHT - height,
       BAR_WIDTH, height - TEXT_GAP);
@@ -51,8 +50,6 @@ var getMaxElement = function (arr) {
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  congratulation(ctx);
-
-  var maxTime = getMaxElement(times);
-
+  renderCongratulation(ctx);
+  renderHistograms(ctx, players, times);
 }
